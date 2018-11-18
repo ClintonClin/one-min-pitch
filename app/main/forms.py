8066e8048010a681a5493789120d603d44964e70
ClintonClin <clintonclin00@gmail.com>
2018-11-18 15:05:17 +0300

    added basic files to make our app function perfectly

---
+++ app/main/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm  # Class to help us create a form class
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from wtforms.validators import Required
+
+
+class PitchForm(FlaskForm):  # inherits from the FlaskForm
+    title = StringField('Pitch title', validators=[Required()])
+    pitch = TextAreaField('Your Pitch', validators=[Required()])
+    my_category = SelectField('Category', choices=[('Technology', 'Technology'), ('Fiction', 'Fiction'), (
+        'Automotive', 'Automotive'), ('Science', 'Science'), ('Trends', 'Trends')], validators=[Required()])
+    submit = SubmitField('Live your life to the fullest!')
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField('Post Comment')
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us more about yourself,', validators=[Required()])
+    submit = SubmitField('Submit')
