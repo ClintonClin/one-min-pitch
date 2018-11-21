cbeaf84040bfb7c1acc18dcf594ad17ee177d664
ClintonClin <clintonclin00@gmail.com>
2018-11-21 12:31:11 +0300

    added a database sqlalchemy link to my config fle

--- config.py
+++ config.py
@@ -1,34 +1,33 @@
-import os  # allow for interaction with the operating system dependent functionality
-
-
+import os
+# allow for interaction with the operating system dependent functionality
 class Config:
     """
-    Describes the general configurations
+    Describes the general configs
     """
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'  # storage location of photos
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos' #fjfj
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clinton:admin@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-    # mail configurations
+    # mail config
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-    # simple mde  configurations
+    # simple mde  config
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 
 
 class ProdConfig(Config):
     """
-    Describes production configuration child class
+    Describes production config of child class
     Args:
-        Config: The parent configration class with General configuration settings
+        Config: The parent configration class with General config settings
     """
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clinton:admin@localhost/pitches'
 
 
 class DevConfig(Config):
@@ -37,11 +36,9 @@ class DevConfig(Config):
     Args:
          Config: The parent configuration class with General configuration settings
     """
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clinton:admin@localhost/pitches'
     DEBUG = True
-
-
-# Dictionary that helps us access the different configuration option classes
+# Dictionary
 config_options = {
     'development': DevConfig,
     'production': ProdConfig
